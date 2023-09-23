dfad4762686da5631068465007fef6dfc3bc5b00
peter varga <pkvarg@yahoo.se>
2023-09-23 13:35:05 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='bg-[#000000] h-[100vh]'>
+      <h1 className='text-[#ffffff] text-[50px] text-center pt-[20%]'>
+        Na stránke sa pracuje
+      </h1>
+    </div>
+  )
+}
+
+export default App
